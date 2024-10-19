647bded1457ea48f3b92c67cfad2342e1e14b369
josejuniorpg <yunipgx@protonmail.com>
2024-10-19 14:26:59 -0600

    Base table Invoice.

--- src/components/Invoices/InvoiceGenerator/InvoiceGenerator.tsx
+++ src/components/Invoices/InvoiceGenerator/InvoiceGenerator.tsx
@@ -1,16 +1,26 @@
 'use client';
 
+import { useState } from 'react';
 import { ClientsCompanyFields } from 'components/Invoices/InvoiceGenerator/ClientsCompanyFields';
 import { DropZoneInvoice } from 'components/Invoices/InvoiceGenerator/DropzoneInvoice';
+import { InvoiceDateFields } from 'components/Invoices/InvoiceGenerator/InvoiceDateFields';
+import { NumberInputInvoice } from 'components/Invoices/InvoiceGenerator/NumberInputInvoice';
 import { TextInputInvoice } from 'components/Invoices/InvoiceGenerator/TextInputInvoice';
 import { YourCompanyFields } from 'components/Invoices/InvoiceGenerator/YourCompanyFields';
 import useInvoiceForm from 'forms/useInvoiceForm';
-import { Box, Button, Card, Group, Stack, Table, Text, Title } from '@mantine/core';
-import { InvoiceDateFields } from '@/src/components/Invoices/InvoiceGenerator/InvoiceDateFields';
+import { Box, Button, Card, Group, Stack, Table, Title } from '@mantine/core';
 import classes from './InvoiceGenerator.module.css';
 
 export function InvoiceGenerator() {
   const form = useInvoiceForm();
+  const [numberRow, setNumberRow] = useState('00');
+
+  const addNumberRow = () => {
+    setNumberRow((prevNumberRow) => {
+      const newNumber = (parseInt(prevNumberRow, 10) + 1).toString().padStart(2, '0');
+      return newNumber;
+    });
+  };
 
   const handleSubmit = (values: ReturnType<typeof useInvoiceForm>['values']) => {
     console.log('Form Submitted:', values);
@@ -38,11 +48,25 @@ export function InvoiceGenerator() {
   ];
 
   const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
-      <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
+    <Table.Tr
+      key={element.name}
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Table.Td style={{ flex: 1 }}>
+        <TextInputInvoice placeholder={numberRow} w="100%" />
+      </Table.Td>
+      <Table.Td style={{ flex: 3 }}>
+        <TextInputInvoice placeholder="Item description" w="100%" />
+      </Table.Td>
+      <Table.Td style={{ flex: 1 }}>
+        <NumberInputInvoice placeholder="1" width="100%" />
+      </Table.Td>
+      <Table.Td style={{ flex: 1 }}>
+        <NumberInputInvoice placeholder="$0.00" width="100%" />
+      </Table.Td>
     </Table.Tr>
   ));
 
@@ -73,7 +97,7 @@ export function InvoiceGenerator() {
           </Stack>
         </Group>
 
-        <Box pt={'3rem'} pb={'3rem'}>
+        <Box pt="3rem" pb="3rem">
           <Table
             striped
             stripedColor="deepBlue.0"
@@ -84,11 +108,18 @@ export function InvoiceGenerator() {
             className={classes.invoiceTable}
           >
             <Table.Thead>
-              <Table.Tr>
-                <Table.Th>ID</Table.Th>
-                <Table.Th>Description</Table.Th>
-                <Table.Th>Quantity</Table.Th>
-                <Table.Th>Price</Table.Th>
+              <Table.Tr
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Table.Th style={{ flex: 1 }}>ID</Table.Th>
+                <Table.Th style={{ flex: 3 }}>Description</Table.Th>
+                <Table.Th style={{ flex: 1 }}>Quantity</Table.Th>
+                <Table.Th ta="center" style={{ flex: 1 }}>
+                  Price
+                </Table.Th>
               </Table.Tr>
             </Table.Thead>
             <Table.Tbody>{rows}</Table.Tbody>
