4e3a83cee771c9569caab76d150491442b2c8d78
josejuniorpg <yunipgx@protonmail.com>
2024-10-19 00:20:00 -0600

    Add right button  inputs.

--- src/components/Invoices/InvoiceGenerator/InvoiceGenerator.tsx
+++ src/components/Invoices/InvoiceGenerator/InvoiceGenerator.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { DropZoneInvoice } from 'components/Invoices/InvoiceGenerator/DropzoneInvoice';
 import { TextInputInvoice } from 'components/Invoices/InvoiceGenerator/TextInputInvoice';
 import { Box, Button, Card, Group, Stack, Text, Title } from '@mantine/core';
+import { theme } from '@/theme';
 import classes from './InvoiceGenerator.module.css';
 
 export function InvoiceGenerator() {
@@ -14,7 +15,7 @@ export function InvoiceGenerator() {
       <Stack gap={0}>
         <Group align="stretch" justify="space-between" w={'100%'}>
           <Stack
-              gap={0}
+            gap={0}
             style={{
               flexGrow: 1,
             }}
@@ -73,18 +74,26 @@ export function InvoiceGenerator() {
               </Box>
             )}
 
-            <Stack>
+
+            {/* todo: put the actual date dinamicaly */}
+            <Stack align={'flex-end'} gap={0}>
               <Group>
-                <Text>dssasadsad</Text>
-                <Text>dssasadsad</Text>
+                <Text fw={500} fz={16} c={'deepBlue.9'}>
+                  Invoice No:
+                </Text>
+                <TextInputInvoice w={105} isTextRight size="28" placeholder={'###'} isBold />
               </Group>
               <Group>
-                <Text>dssasadsad</Text>
-                <Text>dssasadsad</Text>
+                <Text fw={500} fz={16} c={'deepBlue.9'}>
+                  Invoice Date:
+                </Text>
+                <TextInputInvoice w={105} isTextRight size="28" placeholder={'10/8/2024'} isBold />
               </Group>
               <Group>
-                <Text>dssasadsad</Text>
-                <Text>dssasadsad</Text>
+                <Text fw={500} fz={16} c={'deepBlue.9'}>
+                  Due Date:
+                </Text>
+                <TextInputInvoice w={105} isTextRight size="28" placeholder={'10/8/2024'} isBold />
               </Group>
             </Stack>
           </Stack>
--- src/components/Invoices/InvoiceGenerator/TextInputInvoice.tsx
+++ src/components/Invoices/InvoiceGenerator/TextInputInvoice.tsx
@@ -4,21 +4,26 @@ import classes from './InvoiceGenerator.module.css';
 interface TextInputInvoiceProps extends TextInputProps {
   isBold?: boolean;
   placeholder?: string;
+  size?: string;
+  isTextRight?: boolean;
 }
 
 export function TextInputInvoice({
   isBold = false,
   placeholder = 'Input placeholder',
+  size = '20',
+  isTextRight = false, 
   ...rest
 }: TextInputInvoiceProps) {
   return (
     <TextInput
       classNames={{
-        input: `${classes.invoiceTextInput} ${isBold ? classes.inputBold : ''}`,
+        input: `${classes.invoiceTextInput} ${isBold ? classes.inputBold : ''}
+         ${isTextRight ? classes.inputTextRight : ''}`,
       }}
       variant="unstyled"
       placeholder={placeholder}
-      size={'20'}
+      size={size}
       {...rest}
     />
   );
