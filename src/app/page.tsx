77647be7fdf24b5ca768866fe0f9cdac81f82520
josejuniorpg <yunipgx@protonmail.com>
2024-10-20 12:15:42 -0600

    Footer Component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { FooterLinks } from 'components/FooterLinks';
 import { InvoiceGenerator } from 'components/Invoices/InvoiceGenerator/InvoiceGenerator';
+import { InvoiceSectionDown } from 'components/Invoices/InvoiceSectionDown';
 import { InvoicesSectionHeader } from 'components/Invoices/InvoicesSectionHeader';
 import { Stack } from '@mantine/core';
-import { InvoiceSectionDown } from '@/src/components/Invoices/InvoiceSectionDown';
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
         <InvoicesSectionHeader />
         <InvoiceGenerator />
         <InvoiceSectionDown />
+        <FooterLinks />
       </Stack>
     </>
   );
